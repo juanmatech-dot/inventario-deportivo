33d6f3a27b3c30560ec0018a228f46455b6cc2f8
Santiago <santiagogonzalezreinoso@gmail.com>
2026-04-23 11:48:43 -0500

    Arreglo flask

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,24 @@ from flask_cors import CORS
 from routes.equipos  import equipos_bp
 from routes.usuarios import usuarios_bp
 
-app = Flask(__name__)
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(base_dir, '../frontend')
+
+app = Flask(__name__, template_folder=template_dir)
+
 app.secret_key = "bienestar_universitario_2024"
 CORS(app, supports_credentials=True)
 
 app.register_blueprint(equipos_bp)
 app.register_blueprint(usuarios_bp)
 
+from flask import render_template
+
 @app.route("/")
 def index():
-    return {"mensaje": "Sistema de Préstamos Bienestar Universitario ✓"}
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
